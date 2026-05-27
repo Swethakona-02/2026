415f0d008afbb1a9136ebab1acb08ecb191afb28
Swethakona-02 <swethakona1008@gmail.com>
2026-05-27 22:03:37 +0530

    Create wifi range.py

---
+++ wifi range.py
@@ -0,0 +1,17 @@
+class Solution:
+    def wifiRange(self,s, x):
+        n=len(s)
+        coverage = [0] * (n+ 1)
+        for i in range(n):
+            if s[i] == '1':
+                left = max(0, i - x)
+                right = min(n - 1, i + x)
+                coverage[left] += 1
+                if right + 1 < n:
+                    coverage[right + 1] -= 1
+        curr = 0
+        for i in range(n):
+            curr += coverage[i]
+            if curr <= 0:
+                return False
+        return True
